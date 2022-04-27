9ae4165f4888d17b031d9e980db09cf1c965a7c0
Eshar187 <103638736+Eshar187@users.noreply.github.com>
2022-04-27 13:46:19 +0530

    Update output.py

--- output.py
+++ output.py
@@ -2,13 +2,22 @@ Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 
 ==== RESTART: C:\Users\Dell\AppData\Local\Programs\Python\Python310\tax 2.py ===
+
+Q1   
+
 Enter 1st number=13
 Enter 2nd number=45
 Enter 3rd number=63
 The average is= 40.333333333333336
+
+Q2
+
 Enter the Gross Income=12000000
 Enter the number of Dependents=5
 The Tax to be paid is 2395000.0
+
+Q3
+
 Enter the SID=21108030
 Enter Your Name=Esharpreet Singh
 For Male-Enter M
@@ -20,12 +29,18 @@ Enter the Course Name=metallurgy
 Enter your CGPA=7.5
 The List is created as-[SID,Name,Gender,Course Name,CGPA]
 [21108030, 'Esharpreet Singh', 'M', 'metallurgy', '7.5']
+
+Q4
+
 Enter the marks of 1st Student=35
 Enter the marks of 2nd Student=46
 Enter the marks of 3rd Student=30
 Enter the marks of 4th Student=40
 Enter the marks of 5th Student=39
 The list of marks is [35, 46, 30, 40, 39]
+
+Q5
+
 ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
 ['Red', 'Green', 'White', 'Pink', 'Yellow']
 ['Red', 'Green', 'White', 'P', 'u', 'r', 'p', 'l', 'e', 'Yellow']
