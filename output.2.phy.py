eead146b636d18d5153a6117c2c569e8d656634a
Eshar187 <103638736+Eshar187@users.noreply.github.com>
2022-05-14 22:38:39 +0530

    Add files via upload

---
+++ output.2.phy.py
@@ -0,0 +1,32 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: C:/Users/Dell/Desktop/output.2.txt =================
+35
+egaugnal evitisnes esac a si nohtyP
+a case sensitive 
+Python is object oriented language
+10
+Pythonisacasesensitivelanguage
+Enter your name Esharpreet Singh
+Enter your SID 21108030
+Enter your Department Metallurgy
+Enter your CG 6.5
+Hey, Esharpreet Singh Here! 
+My SID is 21108030 
+I am from Metallurgy and my CGPA is 6.500000
+8
+58
+50
+224 
+ 0
+Enter your string Happy
+No
+Enter length 1 = 9.9
+Enter length 2 = 2
+Enter length 3 = 4.5
+No
+Enter your first number = 4
+Enter your second number = 65
+3
+0b1000101
