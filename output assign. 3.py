183be22471664160e3535c451134ffc015581c67
Eshar187 <103638736+Eshar187@users.noreply.github.com>
2022-06-06 00:01:26 +0530

    Add files via upload

---
+++ output assign. 3.py
@@ -0,0 +1,44 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: C:\Users\Dell\Desktop\New folder\esphy.py ==============
+Enter a number of your choice :3
+Binary equivalent of the number entered: 11
+select an operation to perform: 
+1.ADD
+2.SUBTRACT
+3.MULTIPLY
+4.DIVIDE
+4
+enter 1st number: 6
+enter 2nd number: 3
+The division is 2.0
+35
+Enter Number56
+1540.0
+Enter Number34
+14526.724430199203
+Enter an angle in degrees =23
+Enter another angle in degrees =5
+Enter a number =4
+1.849243422292906
+Enter Number y1: 3
+Enter Number y2:2
+Enter Number x1:5
+Enter Number x2: 3
+0.5
+0 1 2 3 4 
+
+3 4 5 6 7 8 9 
+
+4 7 10 
+
+4 7 10 
+
+
+
+Enter no. of hydrogen atoms: 2
+Enter the no. of carbon atoms: 4
+Enter the no. of oxygen atoms: 6
+The molecular weightof the given molecule is 146.05508
+
