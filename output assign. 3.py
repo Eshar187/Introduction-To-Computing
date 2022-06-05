4d27d4d1f799ba1f1378b67842833c2ad64d8920
Eshar187 <103638736+Eshar187@users.noreply.github.com>
2022-06-06 00:05:46 +0530

    Update output assign. 3.py

--- output assign. 3.py
+++ output assign. 3.py
@@ -2,8 +2,11 @@ Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 
 ============== RESTART: C:\Users\Dell\Desktop\New folder\esphy.py ==============
+#1
 Enter a number of your choice :3
 Binary equivalent of the number entered: 11
+  
+#2
 select an operation to perform: 
 1.ADD
 2.SUBTRACT
@@ -13,20 +16,29 @@ select an operation to perform:
 enter 1st number: 6
 enter 2nd number: 3
 The division is 2.0
+
+#3
+a
 35
+b
 Enter Number56
 1540.0
+c
 Enter Number34
 14526.724430199203
+d
 Enter an angle in degrees =23
 Enter another angle in degrees =5
 Enter a number =4
 1.849243422292906
+e
 Enter Number y1: 3
 Enter Number y2:2
 Enter Number x1:5
 Enter Number x2: 3
 0.5
+
+#4
 0 1 2 3 4 
 
 3 4 5 6 7 8 9 
@@ -36,7 +48,7 @@ Enter Number x2: 3
 4 7 10 
 
 
-
+#5
 Enter no. of hydrogen atoms: 2
 Enter the no. of carbon atoms: 4
 Enter the no. of oxygen atoms: 6
