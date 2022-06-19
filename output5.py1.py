3d1c6c6fc60b50669c67e6a72b7b1d4fe8c6e404
Eshar187 <103638736+Eshar187@users.noreply.github.com>
2022-06-19 20:38:42 +0530

    Update output5.py1.py

--- output5.py1.py
+++ output5.py1.py
@@ -2,8 +2,13 @@ Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 
 = RESTART: C:/Users/Dell/AppData/Local/Programs/Python/Python310/output5.py1.py
+#1  
+  
 Enter the string that you want tu reverse: esharpreet singh
 The reversed string is :  hgnis teerprahse
+ 
+ 
+ #2
 Enter the lower range: 23
 Enter the upper range: 70
 Enter the number: 4
@@ -19,10 +24,14 @@ The number is divisible by:  4 56
 The number is divisible by:  4 60
 The number is divisible by:  4 64
 The number is divisible by:  4 68
+ 
+ #3
 Please Enter the First side of a Triangle: 12
 Please Enter the Second side of b Triangle: 23
 Please Enter the Third side of c Triangle: 12
  The Area of a Triangle is 1553.94
+ 
+ #4
 Enter the number: 5
 
 *
@@ -34,6 +43,8 @@ Enter the number: 5
 ***
 **
 *
+
+#5
 Enter the number of rows: 5
 
 A 
@@ -41,6 +52,8 @@ B C
 D E F 
 G H I J 
 K L M N O 
+
+#6
 Please, Enter the Lower Range value : 3
 Please, Enter the Upper Range Value : 35
 The Prime Numbers in the range are: 
@@ -54,3 +67,46 @@ The Prime Numbers in the range are:
 23
 29
 31
+
+#7  
+0,77,154,231,308,385,462
+
+#8
+Enter The Number: 23
+Enter The Number: 34
+Enter The Number: 45
+Enter The Number: 56
+Enter The Number: 67
+Enter The Number: 78
+Enter The Number: 89
+Enter The Number: 90
+Enter The Number: 12
+Enter The Number: 23
+Positive numbers in [23, 34, 45, 56, 67, 78, 89, 90, 12, 23] are: 
+23 34 45 56 67 78 89 90 12 23 
+Negative numbers in [23, 34, 45, 56, 67, 78, 89, 90, 12, 23] are: 
+
+odd numbers in [23, 34, 45, 56, 67, 78, 89, 90, 12, 23] are: 
+23 45 67 89 23 
+Even numbers in [23, 34, 45, 56, 67, 78, 89, 90, 12, 23] are: 
+34 56 78 90 12 
+ 23  occured  2  times
+
+ 34  occured  1  times
+
+ 45  occured  1  times
+
+ 56  occured  1  times
+
+ 67  occured  1  times
+
+ 78  occured  1  times
+
+ 89  occured  1  times
+
+ 90  occured  1  times
+
+ 12  occured  1  times
+ #9
+Enter the list of words seperated by comma(,):e,r,t,y,t,rrrr,t
+Number of occurrences found in the string: 3
