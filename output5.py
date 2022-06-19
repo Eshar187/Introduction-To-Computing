f24dc1acba34e6c7b82fb3d5195aff14fbb806e4
Eshar187 <103638736+Eshar187@users.noreply.github.com>
2022-06-19 20:36:59 +0530

    Update output5.py

--- output5.py
+++ output5.py
@@ -2,7 +2,11 @@ Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 
 =============== RESTART: C:\Users\Dell\Desktop\New folder\loop.py ==============
+  
+#7  
 0,77,154,231,308,385,462
+
+#8
 Enter The Number: 23
 Enter The Number: 34
 Enter The Number: 45
@@ -38,22 +42,6 @@ Even numbers in [23, 34, 45, 56, 67, 78, 89, 90, 12, 23] are:
  90  occured  1  times
 
  12  occured  1  times
-Enter the list of words seperated by comma(,):esh,ar,preet,singh
-{'esh': 1, 'ar': 1, 'preet': 1, 'singh': 1}
-
-=============== RESTART: C:\Users\Dell\Desktop\New folder\loop.py ==============
-Enter the list of words seperated by comma(,):esh,se,se,ter,tre
-tre
-
-
-=============== RESTART: C:\Users\Dell\Desktop\New folder\loop.py ==============
-Enter the list of words seperated by comma(,):es,e,d,e,s,s,
-1
-
-=============== RESTART: C:\Users\Dell\Desktop\New folder\loop.py ==============
-Enter the list of words seperated by comma(,):e
-Number of occurrences found in the string: 1
-
-=============== RESTART: C:\Users\Dell\Desktop\New folder\loop.py ==============
+ #9
 Enter the list of words seperated by comma(,):e,r,t,y,t,rrrr,t
 Number of occurrences found in the string: 3
