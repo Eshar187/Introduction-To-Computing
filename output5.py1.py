b4e059237789be18f0c18df66993e1da48e43bcc
Eshar187 <103638736+Eshar187@users.noreply.github.com>
2022-06-19 20:28:06 +0530

    Add files via upload

---
+++ output5.py1.py
@@ -0,0 +1,56 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Dell/AppData/Local/Programs/Python/Python310/output5.py1.py
+Enter the string that you want tu reverse: esharpreet singh
+The reversed string is :  hgnis teerprahse
+Enter the lower range: 23
+Enter the upper range: 70
+Enter the number: 4
+The number is divisible by:  4 24
+The number is divisible by:  4 28
+The number is divisible by:  4 32
+The number is divisible by:  4 36
+The number is divisible by:  4 40
+The number is divisible by:  4 44
+The number is divisible by:  4 48
+The number is divisible by:  4 52
+The number is divisible by:  4 56
+The number is divisible by:  4 60
+The number is divisible by:  4 64
+The number is divisible by:  4 68
+Please Enter the First side of a Triangle: 12
+Please Enter the Second side of b Triangle: 23
+Please Enter the Third side of c Triangle: 12
+ The Area of a Triangle is 1553.94
+Enter the number: 5
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+Enter the number of rows: 5
+
+A 
+B C 
+D E F 
+G H I J 
+K L M N O 
+Please, Enter the Lower Range value : 3
+Please, Enter the Upper Range Value : 35
+The Prime Numbers in the range are: 
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
