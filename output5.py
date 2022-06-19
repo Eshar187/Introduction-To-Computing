839c13a272d37bb30ed279efffec28bf6d3a8f5d
Eshar187 <103638736+Eshar187@users.noreply.github.com>
2022-06-19 20:34:35 +0530

    Add files via upload

---
+++ output5.py
@@ -0,0 +1,59 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:\Users\Dell\Desktop\New folder\loop.py ==============
+0,77,154,231,308,385,462
+Enter The Number: 23
+Enter The Number: 34
+Enter The Number: 45
+Enter The Number: 56
+Enter The Number: 67
+Enter The Number: 78
+Enter The Number: 89
+Enter The Number: 90
+Enter The Number: 12
+Enter The Number: 23
+Positive numbers in [23, 34, 45, 56, 67, 78, 89, 90, 12, 23] are: 
+23 34 45 56 67 78 89 90 12 23 
+Negative numbers in [23, 34, 45, 56, 67, 78, 89, 90, 12, 23] are: 
+
+odd numbers in [23, 34, 45, 56, 67, 78, 89, 90, 12, 23] are: 
+23 45 67 89 23 
+Even numbers in [23, 34, 45, 56, 67, 78, 89, 90, 12, 23] are: 
+34 56 78 90 12 
+ 23  occured  2  times
+
+ 34  occured  1  times
+
+ 45  occured  1  times
+
+ 56  occured  1  times
+
+ 67  occured  1  times
+
+ 78  occured  1  times
+
+ 89  occured  1  times
+
+ 90  occured  1  times
+
+ 12  occured  1  times
+Enter the list of words seperated by comma(,):esh,ar,preet,singh
+{'esh': 1, 'ar': 1, 'preet': 1, 'singh': 1}
+
+=============== RESTART: C:\Users\Dell\Desktop\New folder\loop.py ==============
+Enter the list of words seperated by comma(,):esh,se,se,ter,tre
+tre
+
+
+=============== RESTART: C:\Users\Dell\Desktop\New folder\loop.py ==============
+Enter the list of words seperated by comma(,):es,e,d,e,s,s,
+1
+
+=============== RESTART: C:\Users\Dell\Desktop\New folder\loop.py ==============
+Enter the list of words seperated by comma(,):e
+Number of occurrences found in the string: 1
+
+=============== RESTART: C:\Users\Dell\Desktop\New folder\loop.py ==============
+Enter the list of words seperated by comma(,):e,r,t,y,t,rrrr,t
+Number of occurrences found in the string: 3
