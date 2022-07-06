2a5c099771dd83e01535c341a1ec8d6f54fc8d95
Eshar187 <103638736+Eshar187@users.noreply.github.com>
2022-07-06 20:20:34 +0530

    Add files via upload

---
+++ output6.py
@@ -0,0 +1,37 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:\Users\Dell\Desktop\New folder\loop.py ==============
+True
+True
+[1]
+[1, 1]
+[1, 2, 1]
+[1, 3, 3, 1]
+[1, 4, 6, 4, 1]
+[1, 5, 10, 10, 5, 1]
+True
+
+
+
+Student ID: 21108030
+Student Name:  Esharpreet Singh
+
+Student ID: 21108030
+Student Name:  Esharpreet Singh
+
+Student Name:  Esharpreet Singh
+Student Class:  Metta 
+True
+False
+True
+False
+
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+py_solution().threeSum([-25, -10, -7, -3, 2, 4, 8, 10]
+True
+False
+True
+
